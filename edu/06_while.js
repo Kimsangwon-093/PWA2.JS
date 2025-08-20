3a176e7f7bb9e81d47510c36f5a0635d608d3826
Kimsangwon-093 <akg9380@daum.net>
2025-08-20 17:44:36 +0900

    250820(06~07 while~function)

---
+++ edu/06_while.js
@@ -0,0 +1,34 @@
+// //while문
+
+// let cnt = 0;
+
+// while(true){
+// console.log('cnt');
+// cnt++;
+//   // console.log('while문 시작');
+
+// if(cnt >= 3){
+//   break;
+// }
+
+//   // if(){
+//   //   break;
+//   // }
+//   // console.log('while문 끝');
+// }
+
+// let multiPlier = 1;
+// while(multiPlier < 1){
+//   console.log(`2 X ${multiPlier} = ${2 * multiPlier}`);
+//   multiPlier++;
+// }
+
+
+//do~while문
+let multiPlier = 1;
+do{
+console.log(`2 X ${multiPlier} = ${2 * multiPlier}`);
+multiPlier++;
+}while(multiPlier < 10);
+
+ 
---
+++ tng/05_tng.js
@@ -0,0 +1,30 @@
+
+
+// let star = '▣';
+
+// for(let i = 0 ; i < 5 ; i++){
+//   let makedStar='';
+//   for(let z = 0; z < 5 ; z++){
+//      makedStar += star;
+//   }
+//   console.log(`${makedStar}`);
+// }
+
+// let a_01 = '│          │';
+
+// console.log('┏━━━━━━━━━━┓');
+// for(a_01 = 0 ; a_01 < 10 ; a_01++){
+//   console.log('┃          ┃');
+// }
+// console.log('┗━━━━━━━━━━┛');
+
+// for(let lineCnt = 0 ; lineCnt < 5 ; lineCnt++){
+//   makedStar += '★';
+//   console.log(makedStar);
+// }
+
+let height = 5;
+
+for(let i = 0 ; i < 10 ; i++){ 
+
+}
